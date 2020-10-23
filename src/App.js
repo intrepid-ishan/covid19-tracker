8d67af1097deafa482bda13c5d8cd5fe39e4e07c
Ishan Makadia <coderishan@gmail.com>
2020-10-23 16:38:05 +0530

    Flow done

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
 import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-import {fetchData} from './api';
+import { fetchData } from './api';
 import coronaImage from './images/image.png';
 
 class App extends React.Component {
 
-    //we dont wanna mess up by sending data in all direction
-    //so therefore getting response of api in App.js and sending
-    //to components through App.js, alternatively we could have tried
-    //to send data directly to Components
     state = {
-        data : {},
-        country : '',
-    }
-    
-    handleCountryChange = async (country) =>{
-        //fetch the data
-        //set the state
-        const fetchedData = await fetchData(country);
-        this.setState({ data : fetchedData });
-        this.setState({country: country});
-    }
-    
-    //componentDidMount called after render
-    async componentDidMount(){
-        const fetchedData = await fetchData();
-        this.setState({ data : fetchedData });
-    }
-    
-    render(){
-        return (
+        data: {},
+        country: '',
+    };
 
+    handleCountryChange = async (country) => {
+        console.log("$$$          start: HCC           $$$");
+        const fetchedData = await fetchData(country);
+        console.log("$$$   mid: HCC stateWillUpdate    $$$");
+        this.setState({ data: fetchedData });
+        console.log("$$$   mid: HCC stateWillUpdate    $$$");
+        this.setState({ country: country });
+        console.log("$$$          end: HCC             $$$");
+    };
+
+    async componentDidMount() {
+        console.log("%%%        start: CDM          %%%");
+        const fetchedData = await fetchData();
+        console.log("%%%   mid: CDM stateWillUpdate %%%");
+        this.setState({ data: fetchedData });
+        console.log("%%%          end: CDM          %%%");
+    }
+
+    render() {
+        console.log("!!!      App Component rendered      !!!");
+        console.log("!!! Card, CP, Chart will be rendered !!!");
+        return (
             <div className={styles.container}>
 
-            <img className={styles.image} src={coronaImage} alt="COVID-19" />
-            <Cards data={this.state.data}/> 
-            <CountryPicker handleCountryChange={this.handleCountryChange} />
-            {/* after render in chart component it will call useeffect */}
-            <Chart data={this.state.data} country={this.state.country}/>
-            
-            </div>
-        )
-    }
+                <img className={styles.image} src={coronaImage} alt="COVID-19" />
+                <Cards data={this.state.data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={this.state.data} country={this.state.country} />
 
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 import styles from './CountryPicker.module.css';
-//##function
-import {fetchCountries} from '../../api';
+import { fetchCountries } from '../../api';
 
 
-const CountryPicker = ( {handleCountryChange} ) => {
+const CountryPicker = ({ handleCountryChange }) => {
 
+    console.log("########   Inside CP Component  ########");
     const [fetchedCountries, setFetchedCountries] = useState([]);
+    console.log("== 0:      CP useState      ==");
 
+    //Here, useEffect will run only 1 time when component mounts
+    //i.e useEffect will as soon as return statement is completed 
     useEffect(() => {
-        const fetchAPI = async() => {
-            setFetchedCountries(await fetchCountries()); //sets fetchedCountries
-        }
-        
+        const fetchAPI = async () => {
+            console.log("== ** 2.1: CP useEffect Before Fetching Countries (stateWillUpdate) ** ==");
+            setFetchedCountries(await fetchCountries());
+            console.log("== ** 2.2:  CP useEffect After Fetching Countries  (stateUpdated)   ** ==");
+        };
         fetchAPI();
-    }, [setFetchedCountries]);
-    
-    //1.05 setFetchedCountries enable us to pick countries 
-    //  console.log(fetchedCountries);
-    
+    }, []);
 
-    //for each country create option and react rule to have key
+    console.log("== 1:      CP Return        ==");
     return (
         <FormControl className={styles.formControl}>
-         <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-             {/* <option value="global">G</option> */}
-             <option value="">Global</option>
-             {fetchedCountries.map((country,i) => <option key={i} value={country}>{country}</option>)}
-         </NativeSelect>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+                {/* for each country create option and react rule is to have key */}
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
         </FormControl>
-    )
-}
+    );
+};
 
 export default CountryPicker;
